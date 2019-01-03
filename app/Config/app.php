a57d97f4db517b7171d3043253c947fe7b8fa540
wjh <wjhokey@gmail.com>
2019-01-03 20:27:35 +0800

    update app config

--- app/Config/app.php
+++ app/Config/app.php
@@ -1,15 +1,18 @@
 <?php
 return [
-    // dev, live
-    'env' => 'dev',
+    'debug' => true,
     // 应用名称
     'app_name' => 'wjh',
     // 视图目录
     'view_dir' => APP_ROOT. 'app/templates',
     // 日志文件
-    'log_file' => APP_ROOT. 'log/wjh.log',
+    'log_file' => APP_ROOT. 'log/wjh-skeleton.log',
     // 缓存目录
     'cache_dir' => APP_ROOT. 'cache',
     // 网站根目录
-    "base_url" => 'http://localhost:8080/',
+    "base_url" => 'http://localhost:8080',
+    // 错误发送邮件
+    "error_send_mail" => true,
+    // 错误日志接收邮箱
+    "error_mail" => 'receive@163.com',
 ];
